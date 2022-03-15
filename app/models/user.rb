e17cbe444f66c7a7aee86b3afa81c213533f6df1
Norman Lau <nlau@ucol.mx>
2022-03-15 00:19:06 -0600

    Task 5: Build Message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 15 }
+  has_many :messages
   has_secure_password
 end
