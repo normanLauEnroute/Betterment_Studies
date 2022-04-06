e1147c5d634c0de9898b5656e85c97fccc9dc51c
Norman Lau <nlau@ucol.mx>
2022-04-05 19:06:05 -0500

    Add Remove View Friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,16 @@
 class FriendshipsController < ApplicationController
+  def create
+    friend = User.find(params[:friend])
+    # @friendship = Friendship.create(user: current_user, friend: friend)
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "#{friend.first_name} #{friend.last_name} was successfully added to your friends"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to friendships_path
+  end
+  
   def destroy
     friend = User.find(params[:id])
     friendship = Friendship.where(user_id: current_user.id, friend_id: friend.id).first
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'friendships', to: 'users#friendships'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
   end
